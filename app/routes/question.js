62cb8044a68e5a61d5946c3d81359d46d74ca452
Blake Scala <blakescala@gmail.com>
2016-10-28 11:44:27 -0700

    add functionality for adding answers

--- app/routes/question.js
+++ app/routes/question.js
@@ -4,7 +4,18 @@ export default Ember.Route.extend({
   model(params) {
     return Ember.RSVP.hash({
       question: this.store.findRecord('question', params.question_id),
-      answers: this.store.findAll('answer'),
+      answers: this.store.findAll('answer')
     });
+  },
+
+  actions : {
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+    }
   }
 });
