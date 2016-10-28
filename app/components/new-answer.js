584126084096744c55c298b99114ea84b51d7da5
Blake Scala <blakescala@gmail.com>
2016-10-28 11:03:48 -0700

    write new answer form that shows when answer button is clicked

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    showAnswerForm() {
+      this.set('addNewAnswer', true);
+    },
+
+    saveAnswer() {
+      this.set('addNewAnswer', false);
+    }
+  }
 });
