dfc05e9f58a6ce8752dc47eed43194b86c00700b
Blake Scala <blakescala@gmail.com>
2016-11-04 13:56:03 -0700

    sort comments, add styling to question route

--- app/components/comment-list.js
+++ app/components/comment-list.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['rating:desc'],
+
+  sortedAnswers: Ember.computed.sort('question.answers', 'sortBy'),
+
   actions: {
+
+    worst() {
+      this.set('sortBy', ['rating:asc']);
+    },
+
+    best() {
+      this.set('sortBy', ['rating:desc']);
+    },
+
     upvote(answer) {
       this.sendAction('upvote', answer);
     },
