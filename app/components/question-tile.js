226a289ae76e3f76f1668ce1fad7c6cf111947fd
Blake Scala <blakescala@gmail.com>
2016-11-04 08:39:02 -0700

    add functionality for starred questions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  starredQuestions: Ember.inject.service(),
+
+  actions: {
+    starThis(question) {
+      this.get('starredQuestions').add(question);
+      console.log(this.get('starredQuestions').length);
+    }
+  }
 });
